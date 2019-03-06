8caf8826f5eff6a9d770f6ce320a76c4c0ed8cd5
MAJUMDER, INDRANIL <indranil.majumder@intl.att.com>
2019-03-07 03:50:51 +0530

    killbill modifications

--- app/killbill/killbill.component.js
+++ app/killbill/killbill.component.js
@@ -9,6 +9,7 @@ angular.
     	var self = this;
     	// data setup
     	this.groupSchema = [];
+
     	var plainSchema = {};
     	plainSchema.key = 'AccountId';
     	plainSchema.mandatoryCondition = true;
@@ -17,13 +18,76 @@ angular.
     	plainSchema.readonly=true;
     	this.groupSchema.push(plainSchema);
     	plainSchema = {};
+    	plainSchema.key = 'email';
+    	plainSchema.mandatoryCondition = true;
+    	plainSchema.multivalue=false;
+    	plainSchema.type='String';
+    	plainSchema.readonly=true;
+    	this.groupSchema.push(plainSchema);
+    	var plainSchema = {};
+    	plainSchema.key = 'name';
+    	plainSchema.mandatoryCondition = true;
+    	plainSchema.multivalue=false;
+    	plainSchema.type='String';
+    	plainSchema.readonly=true;
+    	this.groupSchema.push(plainSchema);
+    	var plainSchema = {};
+    	plainSchema.key = 'address';
+    	plainSchema.mandatoryCondition = true;
+    	plainSchema.multivalue=false;
+    	plainSchema.type='String';
+    	plainSchema.readonly=true;
+    	this.groupSchema.push(plainSchema);
+    	var plainSchema = {};
+    	plainSchema.key = 'postalCode';
+    	plainSchema.mandatoryCondition = true;
+    	plainSchema.multivalue=false;
+    	plainSchema.type='String';
+    	plainSchema.readonly=true;
+    	this.groupSchema.push(plainSchema);
+    	var plainSchema = {};
+    	plainSchema.key = 'company';
+    	plainSchema.mandatoryCondition = true;
+    	plainSchema.multivalue=false;
+    	plainSchema.type='String';
+    	plainSchema.readonly=true;
+    	this.groupSchema.push(plainSchema);
+    	var plainSchema = {};
+    	plainSchema.key = 'city';
+    	plainSchema.mandatoryCondition = true;
+    	plainSchema.multivalue=false;
+    	plainSchema.type='String';
+    	plainSchema.readonly=true;
+    	this.groupSchema.push(plainSchema);
+    	var plainSchema = {};
+    	plainSchema.key = 'state';
+    	plainSchema.mandatoryCondition = true;
+    	plainSchema.multivalue=false;
+    	plainSchema.type='String';
+    	plainSchema.readonly=true;
+    	this.groupSchema.push(plainSchema);
+    	var plainSchema = {};
+    	plainSchema.key = 'country';
+    	plainSchema.mandatoryCondition = true;
+    	plainSchema.multivalue=false;
+    	plainSchema.type='String';
+    	plainSchema.readonly=true;
+    	this.groupSchema.push(plainSchema);
+    	var plainSchema = {};
+    	plainSchema.key = 'phone';
+    	plainSchema.mandatoryCondition = true;
+    	plainSchema.multivalue=false;
+    	plainSchema.type='String';
+    	plainSchema.readonly=true;
+    	this.groupSchema.push(plainSchema);
+    	
+    	plainSchema = {};
     	plainSchema.key = 'basePackage_Selected';
     	plainSchema.mandatoryCondition = true;
     	plainSchema.multivalue=false;
     	plainSchema.type='String';
     	plainSchema.readonly=false;
     	this.groupSchema.push(plainSchema);
-    	
     	plainSchema = {};
     	plainSchema.key = 'currency_Selected';
     	plainSchema.mandatoryCondition = true;
@@ -38,7 +102,6 @@ angular.
     	plainSchema.type='String';
     	plainSchema.readonly=false;
     	this.groupSchema.push(plainSchema);
-    	
     	plainSchema = {};
     	plainSchema.key = 'paymentMethod_Selected_External';
     	plainSchema.mandatoryCondition = true;
@@ -55,8 +118,18 @@ angular.
     		this.user.plainAttrs[key]={};
     		this.user.plainAttrs[key].values=[];
     	}
-    	// test data
-    	this.user.plainAttrs['AccountId'].values=["1234"];
+    	// init data
+    	this.user.plainAttrs['AccountId'].values=["NA"];
+    	this.user.plainAttrs['email'].values=["NA"];
+    	this.user.plainAttrs['address'].values=["NA"];
+    	this.user.plainAttrs['phone'].values=["NA"];
+    	this.user.plainAttrs['postalCode'].values=["NA"];
+    	this.user.plainAttrs['country'].values=["NA"];
+    	this.user.plainAttrs['state'].values=["NA"];
+    	this.user.plainAttrs['city'].values=["NA"];
+    	this.user.plainAttrs['name'].values=["NA"];
+    	this.user.plainAttrs['company'].values=["NA"];
+    	this.user.username="demo@newremmedia.com";
     	
     	var details = KillBillService.getConfig("killbill");
     	var req = {
@@ -69,7 +142,7 @@ angular.
     	//[{key:'bundle1', content:'Lite package'}, {key:'bundle2', content:'Ultimate Package'}];
     	this.availableOptions.basePackages=[];
     	this.availableOptions.currencys = [];
-    	this.availableOptions.amoutn = 'Please select currency';
+    	this.availableOptions.amount = 'Please select currency';
     	
     	//[{key:'paypal', content:'Paypal'}, {key:'stripe', content:'Stripe'}];
     	this.availableOptions.paymentMethods=[];
@@ -79,15 +152,14 @@ angular.
     	this.user.killbillConfig = KillBillService.getConfig();
     	KillBillService.getBaseAvailablePackages(req).then(function(data) {
     	    var pkgList = data;
-    	    console.log(pkgList)
         	for (var i = 0 , len=pkgList.length; i < len ; i++) {
-        		var expr = pkgList[i].product+ " -- " + pkgList[i].plan + " -- " + pkgList[i].finalPhaseBillingPeriod;
+        		var expr = pkgList[i].product+ "/" + pkgList[i].plan + "/" + pkgList[i].priceList + "/" + pkgList[i].finalPhaseBillingPeriod;
         		self.availableOptions.basePackages.push({key: expr, content: expr});
         		self.currencys[expr] = [];        		
         		for (var j = 0 , len2= pkgList[i].finalPhaseRecurringPrice.length; j < len2 ; j++) {
         			//self.availableOptions.basePackages.push({key: pkgList[i].product, content: pkgList[i].product+ " -- " + pkgList[i].plan + " -- " + pkgList[i].finalPhaseBillingPeriod + " -- " + pkgList[i].finalPhaseRecurringPrice[j].currency + " -- " + pkgList[i].finalPhaseRecurringPrice[j].value});
         			self.currencys[expr].push(pkgList[i].finalPhaseRecurringPrice[j].currency);
-        			var id = expr + ' -- ' + pkgList[i].finalPhaseRecurringPrice[j].currency;
+        			var id = expr + '/' + pkgList[i].finalPhaseRecurringPrice[j].currency;
         			self.amount[id] = pkgList[i].finalPhaseRecurringPrice[j].value;
         		}
         	}
@@ -103,10 +175,10 @@ angular.
 
     	KillBillService.getPaymentMethods(req).then(function(data) {
     		var payList = data
-        	console.log(payList)
         	for (var i = 0 , len=payList.length; i < len ; i++) {
-        		//self.availableOptions.paymentMethods.push({key: payList[i].paymentMethodId, content: payList[i].pluginName});
-        		self.availableOptions.paymentMethods.push({key: payList[i].pluginName, content: payList[i].pluginName});
+        		var ele = {key: payList[i].pluginName, content: payList[i].pluginName};
+        		if (self.availableOptions.paymentMethods.indexOf(ele)==-1)
+        			self.availableOptions.paymentMethods.push(ele);
         	}
     	}, function(err) {
     		console.log(err);
@@ -132,13 +204,49 @@ angular.
     		} else if (name  === "currency_Selected") {
     			var selectedCurr = this.user.plainAttrs.currency_Selected.values;
     			//alert(selectedCurr);
-    			//alert(this.amount[selectedPkg +' -- '+ selectedCurr]);
+    			//alert(this.amount[selectedPkg +'/'+ selectedCurr]);
     			this.user.plainAttrs.amount.values=[];
-    			this.user.plainAttrs.amount.values.push(this.amount[selectedPkg +' -- '+ selectedCurr]);
-    			this.user.paypal={};
-    			this.user.paypal.name=this.user.plainAttrs.basePackage_Selected.values[0];
-    			this.user.paypal.currency=this.user.plainAttrs.currency_Selected.values[0]
-    			this.user.paypal.price=this.user.plainAttrs.amount.values[0];    			
+    			this.user.plainAttrs.amount.values.push(this.amount[selectedPkg +"/"+ selectedCurr]);
+    			
+    			this.user.pkgName={};
+    			this.user.pkgName.values=this.user.plainAttrs.basePackage_Selected.values;
+    			this.user.currency={};
+    			this.user.currency.values=this.user.plainAttrs.currency_Selected.values
+    			this.user.price={};
+    			this.user.price.values=this.user.plainAttrs.amount.values;
+    			
+    			//test data
+    	    	this.user.AccountId={};
+    	    	this.user.AccountId.values=["1234"];
+    	    	this.user.email={};
+    	    	this.user.email.values=["demo91@newremmedia.com"];
+    	    	this.user.address={};
+    	    	this.user.address.values=["demo address"];
+    	    	this.user.phone={};
+    	    	this.user.phone.values=["+619999999999"];
+    	    	this.user.postalCode={};
+    	    	this.user.postalCode.values=["MA1234"];
+    	    	this.user.country={};
+    	    	this.user.country.values=["Canada"];
+    	    	this.user.state={};
+    	    	this.user.state.values=["Ontario"];
+    	    	this.user.city={};
+    	    	this.user.city.values=["Toronto"];
+    	    	this.user.name={};
+    	    	this.user.name.values=["New Rem Media"];
+    	    	this.user.company={};
+    	    	this.user.company.values=["New Rem Media"];
+    	    	
+    	    	this.user.plainAttrs['AccountId'].values=["1234"];
+    	    	this.user.plainAttrs['email'].values=["demo91@newremmedia.com"];
+    	    	this.user.plainAttrs['address'].values=["demo address"];
+    	    	this.user.plainAttrs['phone'].values=["+619999999999"];
+    	    	this.user.plainAttrs['postalCode'].values=["MA1234"];
+    	    	this.user.plainAttrs['country'].values=["Canada"];
+    	    	this.user.plainAttrs['state'].values=["Ontario"];
+    	    	this.user.plainAttrs['city'].values=["Toronto"];
+    	    	this.user.plainAttrs['name'].values=["New Rem Media"];
+    	    	this.user.plainAttrs['company'].values=["New Rem Media"];
     		} 		 
     	};
     	
--- app/killbill/killbill.service.js
+++ app/killbill/killbill.service.js
@@ -33,7 +33,7 @@ angular.module('killbill')
             
             killBillService.getBaseAvailablePackages = function (req) { 
 	            return $http(req).then(function(response){
-	            	console.log(response);
+	            	//console.log(response);
 	            	return response.data;
 	            }, error);
 	            //JSON.parse('[ { "product": "Sports", "plan": "sports-monthly", "priceList": "DEFAULT", "finalPhaseBillingPeriod": "MONTHLY", "finalPhaseRecurringPrice": [ { "currency": "GBP", "value": 375 }, { "currency": "USD", "value": 500 } ] }, { "product": "Standard", "plan": "standard-monthly", "priceList": "DEFAULT", "finalPhaseBillingPeriod": "MONTHLY", "finalPhaseRecurringPrice": [ { "currency": "GBP", "value": 75 }, { "currency": "USD", "value": 100 } ] } ]');
@@ -41,14 +41,14 @@ angular.module('killbill')
             
             killBillService.getPaymentMethods = function (req) {
              return $http(req).then(function(response){
-            	 console.log(response);
+            	 //console.log(response);
             	 return response.data;
              }, error);
              //JSON.parse('[ { "paymentMethodId": "916619a4-02bb-4d3d-b3da-2584ac897b19", "externalKey": "coolPaymentMethod", "accountId": "84c7e0d4-a5ed-405f-a655-3ed16ae19997", "isDefault": false, "pluginName": "__EXTERNAL_PAYMENT__", "pluginInfo": null, "auditLogs": [] }, { "paymentMethodId": "dc89832d-18a3-42fd-b3be-cac074fddb36", "externalKey": "paypal", "accountId": "ca15adc4-1061-4e54-a9a0-15e773b3b154", "isDefault": false, "pluginName": "killbill-paypal-express", "pluginInfo": null, "auditLogs": [] } ]');
             };            
             
             killBillService.getConfig = function () {
-            	return JSON.parse('{ "baseUrl": "http://35.185.74.215:8080/1.0/kb", "paymentMethodsURL": "/paymentMethods/pagination?offset=0&limit=100&withPluginInfo=false&audit=NONE", "packagesURL": "/catalog/availableBasePlans", "headers": { "accept": "application/json", "authorization": "Basic YWRtaW46cGFzc3dvcmQ=", "X-Killbill-ApiKey": "admin", "X-Killbill-ApiSecret": "password" }, "stripeURL": "http://35.185.74.215:8080/charge", "stripePublicKey": "pk_test_CmztJP46fhZLqKzLJGzRhfwC", "paypalURL":"https://www.sandbox.paypal.com/cgi-bin/webscr", "paypalInvoiceURL": "http://35.185.74.215:8080/redirect?externalKey=", "paypalBusinessAccount" : "indranil_055332@yahoo.com" }');
+            	return JSON.parse('{ "baseUrl": "http://35.185.74.215:8080/1.0/kb", "paymentMethodsURL": "/paymentMethods/pagination?offset=0&limit=100&withPluginInfo=false&audit=NONE", "packagesURL": "/catalog/availableBasePlans", "headers": { "accept": "application/json", "authorization": "Basic YWRtaW46cGFzc3dvcmQ=", "X-Killbill-ApiKey": "admin", "X-Killbill-ApiSecret": "password" }, "stripeURL": "http://35.185.74.215:4567/charge", "stripePublicKey": "pk_test_CmztJP46fhZLqKzLJGzRhfwC", "paypalURL":"http://35.185.74.215:4568/redirect"}');
             }
             
             return killBillService;         
